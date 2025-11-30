969e0344bf6f73fce7bf08ac457a4e499256864c
Steve <whiteraysoft1@gmail.com>
2025-11-30 17:40:33 +0300

    Remove storage symlink middleware causing 500 error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,11 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
         
-        // Add storage symlink middleware to all web requests
-        $middleware->web(append: [
-            \App\Http\Middleware\EnsureStorageSymlink::class,
-        ]);
-        
         // Replace default CSRF middleware with custom one that excludes all routes
         $middleware->web(replace: [
             \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class => \App\Http\Middleware\VerifyCsrfToken::class,
